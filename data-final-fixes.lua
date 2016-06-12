7b162762f8905fb4b2c6c09244759361cda6893e
Clarence "Sparr" Risher <sparr0@gmail.com>
2016-06-11 19:16:32 -0700

    small improvements

--- data-final-fixes.lua
+++ data-final-fixes.lua
@@ -2,11 +2,10 @@ require "config"
 
 require "serialize_table"
 
-local data_raw = table.tostring(data.raw)
 data:extend({{
     type = "flying-text",
     name = dummy_entity_name,
     time_to_live = 0,
     speed = 1,
-    order = data_raw    
+    order = table.tostring(data.raw)
 }})
